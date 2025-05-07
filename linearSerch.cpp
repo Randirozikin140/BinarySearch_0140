302b7a3bc61d20ff20de552e3fe1c2b0b074ad44
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-05-07 12:14:26 +0700

    first commit

---
+++ linearSerch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Array to be serched
+int n; // Number of elements in the array
+int i; // indeks of array element
+
+void input()
+{
+    while (true)
+    {
+        cout << "Enter number of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n";
+    }
+    
+};
